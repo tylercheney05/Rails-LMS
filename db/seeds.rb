ae8d9ed86d7de574455bda45f37d4d2925bbcb14
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-08 18:54:16 -0700

    created models and controllers, seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+roles = ['teacher', 'ta', 'student']
+5.times do
+  course = Course.create(
+    name: Faker::Beer.brand
+  )
+  3.times do
+    user = User.create(
+      first_name: Faker::ChuckNorris.fact,
+      last_name: Faker::Kpop.solo
+    )
+    Enrollment.create(
+      role: roles.sample,
+      course_id: course.id,
+      user_id: user.id
+    )
+  end
+end
+puts "Data Seeded"
